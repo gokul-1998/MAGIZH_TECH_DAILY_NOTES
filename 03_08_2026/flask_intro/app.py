a9365e347319b61cfed2caf70a3a74d645425caa
gokul-1998 <gokulakrishnanm1998@gmail.com>
2026-08-06 21:57:40 +0530

    done 06_08

--- 03_08_2026/flask_intro/app.py
+++ 03_08_2026/flask_intro/app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    data={"message": "Hello all india"}
+    return render_template("index.html", data=data)
 
 @app.route("/about")
 def about():
@@ -16,4 +17,4 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
